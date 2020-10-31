d2879eab1196bca68eee954a9da69ccb8ba2fc39
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-10-31 19:28:02 +0700

    add query select

--- pkg/data/local/querys.go
+++ pkg/data/local/querys.go
@@ -23,3 +23,32 @@ func QueryInsert(db *sql.DB, table string, data map[string]string) (string, erro
 
 	return "insert success", nil
 }
+
+func QueryUpdate(db *sql.DB, table string, data map[string]string, wheredata map[string]string) (string, error) {
+	sets := []string{}
+	wheres := []string{}
+	for key, value := range data {
+		sets = append(sets, key+" = "+value)
+	}
+	for key, value := range wheredata{
+		wheres = append(wheres, key+" = "+value)
+	}
+    log.Println("update "+table+" set "+strings.Join(sets, ", ")+" where "+strings.Join(wheres, ", "))
+	update, err := db.Query("update "+table+" set "+strings.Join(sets, ", ")+" where "+strings.Join(wheres, ", "))
+	if err != nil {
+		log.Fatal(err)
+		return "update gagal", err
+	}
+
+	defer update.Close()
+
+	return "update success", nil
+}
+
+func QuerySelect(db *sql.DB, table string, data []string) (*sql.Rows) {
+	rows, err := db.Query("select "+strings.Join(data, ", ")+" from "+table)
+	if (err != nil){
+		log.Fatal(err)
+	}
+	return rows
+}
