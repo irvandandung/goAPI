e200d90434e55023ec84346e693695f0d48ff5fd
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-11-01 13:11:58 +0700

    add struct model Buku

--- pkg/data/models.go
+++ pkg/data/models.go
@@ -11,8 +11,22 @@ type ResponseUser struct {
 	Data    []Users
 }
 
+type ResponseBuku struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []Buku
+}
+
 type Users struct {
-	Id        string `form:"id" json:"id"`
+	Id        int `form:"id" json:"id"`
 	Username string `form:"username" json:"username"`
 	Password  string `form:"password" json:"password"`
+}
+
+type Buku struct {
+	Id int `form:"id" json:"id"`
+	Judul string `form:"judul" json:"judul"`
+	Keterangan string `form:"keterangan" json:"keterangan"`
+	Pencipta string `form:"pencipta" json:"pencipta"`
+	Tahun string `form:"tahun" json:"tahun"`
 }
