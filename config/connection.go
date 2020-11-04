2f8892f8ae40a0447906c5c1f752c122c2963958
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-11-04 11:14:16 +0700

    get data conf db from file .env

--- config/connection.go
+++ config/connection.go
@@ -4,11 +4,46 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"fmt"
+	"strings"
 	"time"
+	"github.com/joho/godotenv"
+    "os"
 )
 
 func ConnectDB() *sql.DB {
-	db, err := sql.Open("mysql", "your_username:your_password@tcp(your_IP:3306)/your_database")
+	godotenvErr := godotenv.Load()
+	if godotenvErr != nil {
+	   log.Fatal("Error loading .env file")
+	}
+	errorList := []string{}
+	errorFormat := "ERROR - %s is not found in environment variable"
+	HostDatabase := os.Getenv("HOST_DATABASE")
+	if HostDatabase == "" {
+		errorList = append(errorList, fmt.Sprintf(errorFormat, "HOST_DATABASE"))
+	}
+	PortDatabase := os.Getenv("PORT_DATABASE")
+	if PortDatabase == "" {
+		errorList = append(errorList, fmt.Sprintf(errorFormat, "PORT_DATABASE"))
+	}
+	UserDatabase := os.Getenv("USER_DATABASE")
+	if UserDatabase == "" {
+		errorList = append(errorList, fmt.Sprintf(errorFormat, "USER_DATABASE"))
+	}
+	PasswordDatabase := os.Getenv("PASSWORD_DATABASE")
+	if PasswordDatabase == "" {
+		errorList = append(errorList, fmt.Sprintf(errorFormat, "PASSWORD_DATABASE"))
+	}
+	NameDatabase := os.Getenv("NAME_DATABASE")
+	if NameDatabase == "" {
+		errorList = append(errorList, fmt.Sprintf(errorFormat, "NAME_DATABASE"))
+	}
+	if(len(errorList) != 0){
+		errorMessage := strings.Join(errorList, "\n")
+		log.Println(errorMessage)
+		os.Exit(1)
+	}
+	db, err := sql.Open("mysql", UserDatabase+":"+PasswordDatabase+"@tcp("+HostDatabase+":"+PortDatabase+")/"+NameDatabase)
 	if err != nil {
 		log.Fatal(err)
 	}
