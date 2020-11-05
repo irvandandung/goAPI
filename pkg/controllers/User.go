f9a828c81ac130da6498171cefa6e5d7069f554e
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-11-05 14:06:21 +0700

    add route user for get all data user

--- pkg/controllers/User.go
+++ pkg/controllers/User.go
@@ -22,6 +22,8 @@ var JWT_SIGNING_METHOD = jwt.SigningMethodHS256
 var JWT_SIGNATURE_KEY = []byte("signature of my API")
 
 func Login(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    
 	if r.Method != "POST" {
         http.Error(w, "Unsupported http method", http.StatusBadRequest)
         return
@@ -60,4 +62,58 @@ func Login(w http.ResponseWriter, r *http.Request){
     }
 
     json.NewEncoder(w).Encode(map[string]string{"token": signedToken})
+}
+
+func GetMyDataProfile(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+
+    userInfo := r.Context().Value("userInfo").(jwt.MapClaims)
+    if r.Method != "GET" {
+        http.Error(w, "Unsupported http method", http.StatusBadRequest)
+        return
+    }
+
+    response := data.Response{
+        Status : 200,
+        Message : "Success",
+        Data : userInfo["User"],
+    }
+    json.NewEncoder(w).Encode(response)
+}
+
+func GetAllDataUser(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+
+    userInfo := UserInfo(r.Context().Value("userInfo").(jwt.MapClaims))
+    if r.Method != "GET" {
+        http.Error(w, "Unsupported http method", http.StatusBadRequest)
+        return
+    }
+    if(userInfo.Role != "superuser"){
+        http.Error(w, "You Can't Access this, because you not superuser", http.StatusBadRequest)   
+        return
+    }
+
+    allDataUser := data.GetAllDataUsers()
+
+    response := data.Response{
+        Status : 200,
+        Message : "Success",
+        Data : allDataUser,
+    }
+
+    log.Println(response)
+    json.NewEncoder(w).Encode(response)
+}
+
+func UserInfo (val jwt.MapClaims) data.Users{
+    dataUser := val["User"].(map[string]interface{})
+
+    user := data.Users{
+        Id : dataUser["id"].(float64),
+        Username : dataUser["username"].(string),
+        Role : dataUser["role"].(string),
+    }
+
+    return user
 }
